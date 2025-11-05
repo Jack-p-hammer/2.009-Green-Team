24b295a3dbea8190faf1ad29c5dbb12f4d1c6951
LenaU4087 <helena.usey@gmail.com>
2025-11-04 20:08:15 -0500

    Moteus Motor Driver Integration

--- Boilerplate/include/motor_control.h
+++ Boilerplate/include/motor_control.h
@@ -1,7 +1,38 @@
-#ifndef MOTOR_CONTROL_H
-#define MOTOR_CONTROL_H
+#pragma once
 
-void motor_init();
-void motor_set_speed(float speed);
+#include <Arduino.h>
+#include <ACAN2517FD.h>
+#include <Moteus.h>
 
-#endif
+class MotorControl {
+public:
+    MotorControl();
+
+    void begin();          // Initialize CAN, Moteus, etc.
+    void update();         // Called repeatedly in loop
+
+private:
+
+    static const byte MCP2517_SCK = 9 ; // SCK input of MCP2517
+    static const byte MCP2517_SDI =  10 ; // SDI input of MCP2517
+    static const byte MCP2517_SDO =  11 ; // SDO output of MCP2517
+    
+    static const byte MCP2517_CS  = 17 ; // CS input of MCP2517
+    static const byte MCP2517_INT = 7 ; // INT output of MCP2517
+
+
+
+    // Driver objects
+    ACAN2517FD can;
+
+    // Moteus motors
+    Moteus moteus;
+
+    // Timing variables
+    uint32_t nextSendMillis = 0;
+    uint16_t loopCount = 0;
+
+    // Internal helpers
+    void sendCommands();
+    void printStatus();
+};
--- Boilerplate/src/main.cpp
+++ Boilerplate/src/main.cpp
@@ -2,39 +2,49 @@
 #include "motor_control.h"
 #include "sensors.h"
 #include "control_scheme.h"
+#include <Moteus.h>
+#include <ACAN2517FD.h>
+
+
+MotorControl motorControl;
+
 
 // Sampling period (ms), might have to adjust this
-const unsigned long LOOP_INTERVAL = 10; 
-unsigned long lastLoopTime = 0;
+// const unsigned long LOOP_INTERVAL = 10; 
+// unsigned long lastLoopTime = 0;
 
 void setup() {
-  Serial.begin(115200);
 
-  motor_init();
-  sensors_init();
-  control_init();
+  motorControl.begin();
+
+  // sensors_init();
+  // control_init();
 
 }
 
 void loop() {
-  unsigned long now = millis();
-  if (now - lastLoopTime >= LOOP_INTERVAL) {
-    lastLoopTime = now;
+  
+  motorControl.update();
+
+  // unsigned long now = millis();
+  // if (now - lastLoopTime >= LOOP_INTERVAL) {
+  //   lastLoopTime = now;
 
     // Read sensors
-    float force = read_force_sensor();
-    float linearPos = read_linear_encoder();
-    float rotation = read_rotational_encoder();
+  
+    // float force = read_force_sensor();
+    // float linearPos = read_linear_encoder();
+    // float rotation = read_rotational_encoder();
 
     // Print sensor readings
-    Serial.println(force);
-    Serial.println(linearPos);
-    Serial.println(rotation);
+    // Serial.println(force);
+    // Serial.println(linearPos);
+    // Serial.println(rotation);
 
     // Compute control output
-    float controlOutput = compute_control(force, linearPos, rotation);
+    //float controlOutput = compute_control(force, linearPos, rotation);
 
     // Send command to motor driver
-    motor_set_speed(controlOutput);
-  }
+    //motor_set_speed(controlOutput);
+  //}
 }
