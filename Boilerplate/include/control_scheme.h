483d61020d9d7779e7e95cb0b064a52c441d15c4
gliksber <75272895+dgliksberg@users.noreply.github.com>
2025-11-08 19:41:22 -0500

    Removed boilerplate PIO project

--- Boilerplate/include/control_scheme.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONTROL_SCHEME_H
-#define CONTROL_SCHEME_H
-
-void control_init();
-float compute_control(float force, float linearPos, float rotation);
-
-#endif
--- Boilerplate/include/sensors.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSORS_H
-#define SENSORS_H
-
-void sensors_init();
-float read_force_sensor();
-float read_linear_encoder();
-float read_rotational_encoder();
-
-#endif
