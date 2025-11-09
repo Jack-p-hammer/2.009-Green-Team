7722593b6401ac87fb35ef88931ad023b9fc00af
gliksber <75272895+dgliksberg@users.noreply.github.com>
2025-11-08 23:42:36 -0500

    added base for HMI code/env

---
+++ CPR Machine/src/HMI/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <Bounce2.h> // Button Debouncing
+
+void setup() {
+    // Code to run once on board reset here
+}
+
+void loop() {
+    // Code to run on repeat here
+}
